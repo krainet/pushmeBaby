7687ef4b437a346aee9961237fecbfdf09ec0ac8
Ramón Albertí <krainet@gmail.com>
2015-09-10 11:10:44 +0200

    First commit

---
+++ src/common/config/config.js
@@ -0,0 +1,19 @@
+/* 
+ * Configuration File
+ */
+
+//URL from api to test api resources
+//var API_URL = 'http://validate.jsontest.com/';
+var API_URL = 'http://jsonplaceholder.typicode.com';
+
+//Send custom header in your request and response headers
+//Used for example to send auth-cookies in headers
+var USE_CUSTOM_HEADER = false;
+var CUSTOM_HEADER = 'Accept-Language';
+
+
+//To test login with facebook you need to setup pushmeBaby.com as your local virtualhost
+//to obtain login responses , try it!
+var LOGIN_FACEBOOK_REDIRECT_URI = "http://pushmeBaby.com:8080/auth/facebook/";
+var LOGIN_FACEBOOK_CLIENT_ID    = "723035201084388";
+var LOGIN_FACEBOOK_SCOPE        = ["email"];
