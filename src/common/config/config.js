12aec38b3c97719b46264330c5dc063256942fb5
Ramón Albertí <krainet@gmail.com>
2015-09-10 13:49:52 +0200

    Fix https dummy api

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -4,7 +4,7 @@
 
 //URL from api to test api resources
 //var API_URL = 'http://validate.jsontest.com/';
-var API_URL = 'http://jsonplaceholder.typicode.com';
+var API_URL = 'https://jsonplaceholder.typicode.com';
 
 //Send custom header in your request and response headers
 //Used for example to send auth-cookies in headers
