398c5df65a157e01dd5b2839c1d5add2ecd9dd6d
Ramón Albertí <ralberti@mequedouno.com>
2015-09-25 11:13:05 +0200

    Drag & Drop

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -2,32 +2,78 @@
     app.config(['$stateProvider',
         function ($stateProvider) {
             $stateProvider
-                    .state('root.about', {
-                        url: '/about',
-                        parent: 'root',
-                        views: {
-                            "container@": {
-                                controller: 'AboutController',
-                                templateUrl: 'about/about.tpl.html'
-                            }
-                        },
-                        data: {
-                            pageTitle: 'About'
+                .state('root.about', {
+                    url: '/about',
+                    parent: 'root',
+                    views: {
+                        "container@": {
+                            controller: 'AboutController',
+                            templateUrl: 'about/about.tpl.html'
                         }
-                    });
+                    },
+                    data: {
+                        pageTitle: 'About'
+                    }
+                });
         }]);
 
     app.controller('AboutController', ['$scope', '$log','$state', function ($scope, $log,$state) {
-            $log.info('App:: Starting AboutController');
+        $log.info('App:: Starting AboutController');
 
-            var init = function () {
-                $scope.model = {};
-                $scope.model.pageTitle = $state.current.data.pageTitle;
+        var init = function () {
+            $scope.model = {};
+            $scope.model.pageTitle = $state.current.data.pageTitle;
 
-            };
-            init();
-        }]);
+        };
+        init();
+        $scope.dragoverCallback = function(event, index, external, type) {
+            $scope.logListEvent('dragged over', event, index, external, type);
+            // Disallow dropping in the third row. Could also be done with dnd-disable-if.
+            return index < 10;
+        };
+
+        $scope.dropCallback = function(event, index, item, external, type, allowedType) {
+            $scope.logListEvent('dropped at', event, index, external, type);
+            if (external) {
+                if (allowedType === 'itemType' && !item.label) {return false;}
+                if (allowedType === 'containerType' && !angular.isArray(item)) {return false;}
+            }
+            return item;
+        };
+
+        $scope.logEvent = function(message, event) {
+            console.log(message, '(triggered by the following', event.type, 'event)');
+            console.log(event);
+        };
+
+        $scope.logListEvent = function(action, event, index, external, type) {
+            var message = external ? 'External ' : '';
+            message += type + ' element is ' + action + ' position ' + index;
+            $scope.logEvent(message, event);
+        };
+
+        $scope.model = [];
+
+        // Initialize model
+        var id = 10;
+        for (var i = 0; i < 3; ++i) {
+            $scope.model.push([]);
+            for (var j = 0; j < 2; ++j) {
+                $scope.model[i].push([]);
+                for (var k = 0; k < 7; ++k) {
+                    $scope.model[i][j].push({label: 'Item ' + id++});
+                }
+            }
+        }
+
+        $scope.$watch('model', function(model) {
+            $scope.modelAsJson = angular.toJson(model, true);
+        }, true);
+
+
+    }]);
 
 }(angular.module("pushmeBaby.about", [
-    'ui.router'
+    'ui.router',
+    'dndLists'
 ])));
